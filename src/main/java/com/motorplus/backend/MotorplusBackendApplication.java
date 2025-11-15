66797ff77fed4793b7c340d81e4ca5eea8627e17
helen <helen8335@gmail.com>
2025-11-15 11:40:29 -0500

    Initial commit

---
+++ src/main/java/com/motorplus/backend/MotorplusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.motorplus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorplusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorplusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motorplus/backend/MotorplusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorplus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorplusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
